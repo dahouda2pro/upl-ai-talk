4c5b2b023cdf83f76e8d5a55c54e30f0350bb8a4
dahouda2pro <dahouda37@gmail.com>
2024-05-21 17:51:24 +0900

    updating Footer

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     
 
       <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        © 2024 Copyright:
+      
         <p>Dr. Dahouda MWAMBA@UPL</p>
           
         
