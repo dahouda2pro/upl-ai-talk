665d8245541289b9b26fcae8e860d5d89b1cf568
dahouda2pro <dahouda37@gmail.com>
2024-05-21 17:09:52 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Heater from './component/Heater.js'
+import Footer from './component/Footer.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Heater />
     <App />
+    <Footer />
   </React.StrictMode>
 );
 
